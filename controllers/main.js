843e2cbf2d230b6f20f71862e4ab9c209fbae484
chryzcode <alabaolanrewaju13@gmail.com>
2023-12-20 00:17:29 +0100

    push

--- controllers/main.js
+++ controllers/main.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const CustomAPIError = require("../errors/custom-error");
 
 const login = async (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   const { username, password } = req.body;
   if (!username || !password) {
     throw new CustomAPIError("Please provide your email and password", 400);
@@ -20,18 +20,11 @@ const dashboard = async (req, res) => {
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     throw new CustomAPIError("No token provided", 401);
   }
-
-  const token = authHeader.split(" ")[1]; 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const luckyNumber = Math.floor(Math.random() * 100);
-    res.status(200).json({
-      msg: `Hello, ${(decoded.username)}`,
-      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-    });
-  } catch (error) {
-    throw new CustomAPIError("Not authorized to access this route", 401);
-  }
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
 };
 
 module.exports = {
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,16 @@
 const jwt = require("jsonwebtoken");
 const CustomAPIError = require("../errors/custom-error");
-const authenticationMiddleware = async (req, res, nexe) => {
-  console.log(req.headers.authorization);
-  next();
+const authenticationMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const { id, username } = decoded;
+    req.user = { id, username };
+    next();
+  } catch (error) {
+    throw new CustomAPIError("Not authorized to access this route", 401);
+  }
 };
 
 module.exports = authenticationMiddleware;
