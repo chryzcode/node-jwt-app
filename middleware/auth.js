1b76e6f652e8accfd31d3a2eddd33846a750f70d
chryzcode <alabaolanrewaju13@gmail.com>
2023-12-19 23:58:40 +0100

    commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,8 @@
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
 const authenticationMiddleware = async (req, res, nexe) => {
-    console.log(req.headers.authorization)
-    next()
-}
+  console.log(req.headers.authorization);
+  next();
+};
 
-module.exports = authenticationMiddleware
+module.exports = authenticationMiddleware;
